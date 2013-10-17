472a7587b2ae4d4e829eab6323fdcce4cb538313
Rafael Verger <rafael@rafaelverger.com.br>
2013-10-17 02:28:05 -0300

    Create TODO filter

--- meteor-class.js
+++ meteor-class.js
@@ -3,7 +3,20 @@ Todos = new Meteor.Collection("todos")
 if (Meteor.isClient) {
   // client code
   Template.todo_list.todos = function(){
-    return Todos.find({});
+    var filter = Session.get('filter');
+    if ( !filter ) return Todos.find();
+
+    return Todos.find({desc: {$regex: '^.*'+filter+'.*$', $options: 'i'}});
+  }
+
+  Template.todo_list.todosCount = function(){
+    return Todos.find().fetch().length;
+  }
+
+  Template.todo_list.events = {
+    'keyup #todo_filter': function(evt){
+      Session.set('filter', evt.target.value.trim());
+    }
   }
 
   Template.todo_info.events = {
